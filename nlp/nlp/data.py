71b1aa4eeeb934b087213824bd19cce52d2005f4
Alfio Ferrara <alfio.ferrara@unimi.it>
2024-03-04 10:18:58 +0100

    Examples first class

---
+++ nlp/nlp/data.py
@@ -0,0 +1,33 @@
+"""Collections of methods for gathering data"""
+import pandas as pd
+from sklearn.datasets import fetch_20newsgroups
+from collections import defaultdict
+
+
+
+SKLEARN_FOLDER = "/Users/flint/Data/sklearn"
+
+
+def load_20news(subset: str = 'all', categories: list[str] = None, remove: tuple = ('headers', 'footers', 'quotes')):
+    """Ovverrides 20 News fetching
+
+    Args:
+        subset (str, optional): Select the dataset to load: ‘train’ for the training set, ‘test’ for the test set, 
+        ‘all’ for both, with shuffled ordering. Defaults to 'all'.
+        categories (list[str], optional): If None (default), load all the categories. 
+        If not None, list of category names to load (other categories ignored). Defaults to None.
+        remove (tuple, optional): May contain any subset of (‘headers’, ‘footers’, ‘quotes’). 
+        Defaults to ('headers', 'footers', 'quotes').
+
+    Returns:
+        object: Data bunch as for sklearn
+    """
+    return fetch_20newsgroups(data_home=SKLEARN_FOLDER, subset=subset, categories=categories, remove=remove)
+
+
+def frequency(corpus: list[str], tokenizer: callable):
+    data = defaultdict(lambda: 0)
+    for document in corpus:
+        for token in tokenizer(document):
+            data[token] += 1
+    return pd.Series(data)
